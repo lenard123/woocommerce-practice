6fd82719bfb52f485649cbfb0ea2dcc16671473d
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-07-14 10:32:41 +0800

    WIP

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="bg-slate-100">
     <div class="max-w-screen-xl h-[70vh] mx-auto container overflow-hidden">
-        <img class="w-full h-full object-cover " src="./assets/images/banner3.png" />
+        <img class="w-full h-full object-cover " src="<?= get_theme_file_uri('assets/images/banner3.png') ?>" />
     </div>
 </div>
 
